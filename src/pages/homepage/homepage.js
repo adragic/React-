8d35dc865410edf95a19b2d8216c39d316bab5eb
adragic1 <adragic@etfos.hr>
2020-02-22 23:02:42 +0100

    broken homepage into a smaller components, finished styling menu-item

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./homepage.css";
+import Directory from "../../components/directory/directory";
+
+const HomePage = () => {
+  return (
+    <>
+    <div className="homepage">
+      <Directory />
+
+    </div>
+      
+    </>
+  )
+}
+
+export default HomePage;
